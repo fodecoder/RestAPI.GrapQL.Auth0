9e16bbbd6181f05a9da1912af1f6785c7a8e1775
Andrea Foderaro <fodecoder@gmail.com>
2024-10-22 14:00:40 +0200

    Add common libraries

---
+++ Common.Services/Options/GraphQLOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Services.Options
+{
+    public class GraphQLOptions
+    {
+        public string EndPoint { get; set; }
+    }
+}
---
+++ Common.Services/Requests/GraphQLRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Services.Requests
+{
+    public sealed class GraphQLRequest
+    {
+        public string OperationName { get; set; }
+        public string Query { get; set; }
+        public Dictionary<string , object> Variables { get; set; }
+    }
+}

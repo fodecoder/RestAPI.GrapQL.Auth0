ef9fc4d2e5aa0367aa0e00506e8167b99486d088
Andrea Foderaro <fodecoder@gmail.com>
2024-10-22 15:55:14 +0200

    Add console app to test the APIs

---
+++ RestAPI.GraphQL.Auth0.Client.ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Common.Services.Extension;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RestAPI.GraphQL.Auth0.Client.ConsoleApp;
+using RestAPI.GraphQL.Auth0.Server.BL.Interfaces.ProxyServices;
+using RestAPI.GraphQL.Auth0.Server.BL.Interfaces.Service;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder ( args );
+builder.Configuration.Sources.Clear ();
+
+IHostEnvironment env = builder.Environment;
+
+builder.Configuration
+    .AddJsonFile ( "appsettings.json" , optional: true , reloadOnChange: true )
+    .AddJsonFile ( $"appsettings.{env.EnvironmentName}.json" , true , true );
+
+builder.Services.AddProxy<IItemService , ProxyItemService> ( builder.Configuration );
+builder.Services.AddTransient<Tester> ();
+
+using IHost host = builder.Build ();
+
+await ExemplifyServices ( host.Services );
+
+static async Task ExemplifyServices( IServiceProvider hostProvider )
+{
+    using IServiceScope serviceScope = hostProvider.CreateScope ();
+    IServiceProvider provider = serviceScope.ServiceProvider;
+    Tester tester = provider.GetRequiredService<Tester> ();
+    await tester.Run ();
+
+    Console.WriteLine ();
+}
---
+++ RestAPI.GraphQL.Auth0.Client.ConsoleApp/Tester.cs
@@ -0,0 +1,42 @@
+﻿using RestAPI.GraphQL.Auth0.Server.BL.Interfaces.Model;
+using RestAPI.GraphQL.Auth0.Server.BL.Interfaces.Service;
+
+namespace RestAPI.GraphQL.Auth0.Client.ConsoleApp
+{
+    public class Tester
+    {
+        private readonly IItemService _itemService;
+
+        public Tester( IItemService itemService )
+        {
+            _itemService = itemService;
+        }
+
+        public async Task Run()
+        {
+            Console.WriteLine ( "Testin APIs..." );
+
+            Console.WriteLine ( "Test create..." );
+            await _itemService.CreateItemAsync (
+                new Item ()
+                {
+                    Name = "Test - " + new Random ().Next ( 0 , 10000 ) ,
+                    Description = "Description - " + new Random ().Next ( 0 , 10000 ) ,
+                    Type = "Type - " + new Random ().Next ( 0 , 10 )
+                } );
+
+            Console.WriteLine ( "Test Read..." );
+            await _itemService.ReadItemAsync ( Guid.Parse ( "D71E936A-AABB-4A9D-B26D-0474802E8406" ) );
+
+            Console.WriteLine ( "Test Update..." );
+            await _itemService.UpdateItemAsync (
+                new Item ()
+                {
+                    Id = Guid.Parse ( "D71E936A-AABB-4A9D-B26D-0474802E8406" ) ,
+                    Name = "Test - " + new Random ().Next ( 0 , 10000 ) ,
+                    Description = "Description - " + new Random ().Next ( 0 , 10000 ) ,
+                    Type = "Type - " + new Random ().Next ( 0 , 10 )
+                } );
+        }
+    }
+}

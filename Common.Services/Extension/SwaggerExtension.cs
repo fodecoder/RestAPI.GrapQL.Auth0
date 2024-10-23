66e129bbb1f6dc8da8085bcf8abde4acba1cd1b0
Andrea Foderaro <fodecoder@gmail.com>
2024-10-23 15:23:40 +0200

    Add swagger authentication options

---
+++ Common.Services/Extension/SwaggerExtension.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Common.Services.Extension
+{
+    public static class SwaggerExtension
+    {
+        public static void AddSwagger( this IServiceCollection serviceCollection )
+        {
+            serviceCollection.AddSwaggerGen ( c =>
+            {
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "JWT Authentication" ,
+                    Description = "Enter JWT Bearer token **_only_**" ,
+                    In = ParameterLocation.Header ,
+                    Type = SecuritySchemeType.Http ,
+                    Scheme = "bearer" , // must be lower case
+                    BearerFormat = "JWT" ,
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme ,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+                c.CustomOperationIds ( e => e.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor ?
+                    controllerActionDescriptor.MethodInfo.Name :
+                    e.ActionDescriptor.AttributeRouteInfo?.Name );
+                c.AddSecurityDefinition ( securityScheme.Reference.Id , securityScheme );
+                c.AddSecurityRequirement ( new OpenApiSecurityRequirement
+                  {
+                    {securityScheme, Array.Empty<string>()}
+                  } );
+                //c.OperationFilter<SwaggerEtagHeaderFilter> ();
+                //c.OperationFilter<SwaggerVersionHeaderFilter> ();
+            } );
+            //serviceCollection.AddTransient<IConfigureOptions<SwaggerGenOptions> , SwaggerInfoOptions> ();
+        }
+    }
+}
